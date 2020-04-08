31b0c69282cef42583c3476c6229f4622697e11d
joelaguer14 <46728129+joelaguer14@users.noreply.github.com>
2020-04-08 17:11:10 -0600

    addRow script added, there's a bug when I try to add the image from a link in the form

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log("Hello World from JavaScript");
+//alert("Hello world, welcome to my bakery web page");
+function addRow() {
+    var name = document.getElementById('name').value;
+    var desc = document.getElementById('desc').value;
+    var price = document.getElementById('price').value;
+    var image = document.getElementById('imageURL').textContent;
+    var table = document.getElementsByTagName('table')[0];
+
+    var newRow = table.insertRow(2);
+
+    var cel1 = newRow.insertCell(0);
+    var cel2 = newRow.insertCell(1);
+    var cel3 = newRow.insertCell(2);
+    var cel4 = newRow.insertCell(3);
+
+    var img = document.createElement("img");
+
+    img.src = image;
+
+
+    cel1.innerHTML = name;
+    cel2.innerHTML = desc;
+    cel3.innerHTML = price;
+    cel4.innerHTML = '<img src="' + image + '" alt="picture of ' + name + '">';
+
+}
